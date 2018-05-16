51dd399123824f3cf63343d143a27eda88a3bba1
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2018-05-16 17:06:28 +0200

    added tests, now need to debuug the dynamic matrix

--- src/dynamic_matrix.hpp
+++ src/dynamic_matrix.hpp
@@ -9,6 +9,7 @@
 
 #include <deque>
 #include <array>
+#include <iostream>
 
 // this allows both positive and negative indices
 
@@ -26,24 +27,39 @@ public:
 	T& operator [](std::array<int, 2> idx) {
 		int i = idx[0] - externalRowOffset;
 		int j = idx[1] - externalColOffset;
-		if (i < 0) {
-			internalRowOffset = std::min(internalRowOffset, -1*i);
-		}
-		if (j < 0) {
-			internalColOffset = std::min(internalColOffset, -1*i);
-		}
 
 		while (i + internalRowOffset >= nrows) {
 			std::deque<T> newRow(ncols);
 			rows.push_back(newRow);
 			nrows++;
 		}
-		if (j + internalColOffset >= ncols) {
-			for (int i1 = 0; i1 < nrows; ++i1) {
-				rows[i1].resize(j + internalColOffset + 1);
-			}
-			ncols = j + internalColOffset;
+		while (i <= -1 * internalRowOffset) {
+			std::deque<T> newRow(ncols);
+			rows.push_front(newRow);
+			nrows++;
 		}
+		while (j + internalColOffset >= ncols) { // TODO: THIS CHECK IS WRONG!!!
+			for (int i1 = 0; i1 < nrows; ++i1) {
+				T newEntry = 0;
+				rows[i1].push_back(newEntry);
+			}
+			ncols++;
+		}
+		while (j <= -1 * internalColOffset) { // TODO: THIS CHECK IS WRONG!!!
+			for (int i1 = 0; i1 < nrows; ++i1) {
+				T newEntry = 0;
+				rows[i1].push_front(newEntry);
+			}
+			ncols++;
+		}
+
+		if (i < 0) {
+			internalRowOffset = std::max(internalRowOffset, -1 * i);
+		}
+		if (j < 0) {
+			internalColOffset = std::max(internalColOffset, -1 * j);
+		}
+
 		return rows[i + internalRowOffset][j + internalColOffset];
 	}
 	int getNRows() {
@@ -57,6 +73,14 @@ public:
 	}
 	void setExternalColOffset(int offset) {
 		externalColOffset = offset;
+	}
+	void printMatrix() {
+		for (int i = 0; i < nrows; ++i) {
+			for (int j = 0; j < ncols; ++j) {
+				std::cout << rows[i][j] << " ";
+			}
+			std::cout << "\n";
+		}
 	}
 private:
 	std::deque<std::deque<T> > rows;
